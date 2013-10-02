72b9750df3d37683acc40d744ace19a70e8cd007
Brandon Ewoldt <bewoldt1@gmail.com>
2013-10-02 11:18:11 -0500

    clean up

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,7 +1,7 @@
 
-	<!-- Scripts -->
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+	<script>window.jQuery || document.write('<script src="js/plugins/jquery-1.8.0.min.js"><\/script>')</script>
 	<script src="js/plugins-ck.js"></script>
 	<script src="js/main-ck.js"></script>
 
--- index.php
+++ index.php
@@ -1,18 +1,43 @@
-<?php include 'inc/doctype.php'; ?>
-<?php include 'inc/meta.php'; ?>
-<title>Home | Marketing &amp; Design.</title>
+<!DOCTYPE html>
+<!--[if lt IE 9]>         <html lang="en" class="lt-ie9 no-js"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Home | Student Life Marketing &amp; Design.</title>
+<!-- Google Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
-<!--[if lt IE 9]>
-    <script src="js/vendor/html5shiv.js"></script>
-    <script src="js/vendor/respond.min.js"></script>
-<![endif]-->
+<!-- CSS -->
 <link rel="stylesheet" href="css/master.css">
+<!-- JS -->
+<!--[if lt IE 9]>
+    <script src="js/plugins/html5shiv.js"></script>
+    <script src="js/plugins/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body>
 
 
-    <?php include 'inc/scripts.php'; ?>
+
+	<!-- JS -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/plugins/jquery-1.8.0.min.js"><\/script>')</script>
+	<script src="js/plugins-ck.js"></script>
+	<script src="js/main-ck.js"></script>
+
+	<!-- Google Universal Analytics: change UA-XXXXX-X and test.com.
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-XXXXX-X', 'test.com');
+		ga('send', 'pageview');
+	</script>
+	 -->
 
 </body>
 </html>
