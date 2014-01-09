efed9b1fd5e97a086b144f36eb4cdc0bcedff1aa
Brandon Ewoldt <bewoldt1@gmail.com>
2014-01-09 10:26:36 -0600

    updated file paths.
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- styleguide.php
+++ styleguide.php
@@ -6,10 +6,13 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Styleguide | Student Life Marketing &amp; Design.</title>
-<!-- Google Fonts -->
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+
+<!-- Typekit -->
+<?php include 'inc/typekit.php'; ?>
+
 <!-- CSS -->
 <link rel="stylesheet" href="css/master.css">
+
 <!-- JS -->
 <!--[if lt IE 9]>
     <script src="js/plugins/html5shiv.js"></script>
@@ -588,21 +591,8 @@
 
     <!-- JS -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/plugins/jquery-1.8.0.min.js"><\/script>')</script>
-    <script src="js/plugins-ck.js"></script>
-    <script src="js/main-ck.js"></script>
+    <script src="js/build/production.min.js"></script>
 
-    <!-- Google Universal Analytics: change UA-XXXXX-X and test.com.
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-XXXXX-X', 'test.com');
-        ga('send', 'pageview');
-    </script>
-     -->
 
 </body>
 </html>
