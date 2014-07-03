510e4a811b38bb232718ff64dda482414da31977
Brandon Ewoldt <bewoldt1@gmail.com>
2014-07-03 12:26:48 -0500

    commit
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
 <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Home | Student Life Marketing &amp; Design.</title>
@@ -32,20 +33,17 @@
 	<!-- JS -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/plugins/jquery-1.8.0.min.js"><\/script>')</script>
-	<script src="js/plugins-ck.js"></script>
-	<script src="js/main-ck.js"></script>
+	<script src="js/min/production-min.js"></script>
 
-	<!-- Google Universal Analytics: change UA-XXXXX-X and test.com.
+	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID -->
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-XXXXX-X', 'test.com');
+		ga('create', 'UA-XXXXX-X');
 		ga('send', 'pageview');
 	</script>
-	 -->
 
 </body>
 </html>
